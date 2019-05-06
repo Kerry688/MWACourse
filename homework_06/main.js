10be23da4f889beb09b888c647f3a447f18d9a16
Karim Bayoumi <kerry688@hotmail.com>
2019-05-06 14:50:44 -0500

    exercise

--- homework_06/main.js
+++ homework_06/main.js
@@ -5,11 +5,9 @@ app.use(express.json());
  
 let grades=[
     {id:"1",name:'Assad Saad',course:'MWA',grade:95},
-    {id:"2",name:'Karim Ali',course:'Alogorithm',grade:92},
+    {id:"2",name:'Karim Ali',course:'Alogorithm',grade:92} 
 ]
  
-
-  
 //Get
 app.get('/grades',function(req,res){
     res.send(grades);
@@ -41,8 +39,7 @@ app.delete('/grades/:id',function(req,res){
     res.end();   
 }
 );
-
-
+ 
 
 app.listen(3000, function() 
 {console.log('listening on 3000')});
