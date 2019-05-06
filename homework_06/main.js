c1f11142f2711047fbdcef3eb414bc448971e3fe
Karim Bayoumi <kerry688@hotmail.com>
2019-05-06 14:41:13 -0500

    exercise

---
+++ homework_06/main.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+ 
+ 
+let grades=[
+    {id:"1",name:'Assad Saad',course:'MWA',grade:95},
+    {id:"2",name:'Karim Ali',course:'Alogorithm',grade:92},
+]
+ 
+
+  
+//Get
+app.get('/grades',function(req,res){
+    res.send(grades);
+    res.end();   
+}
+);
+
+app.get('/grades/:id',function(req,res){    
+    const id=req.params.id;
+    const result = grades.find( grade => grade.id == id);
+     res.send(JSON.stringify(result));
+    res.end();   
+}
+);
+
+ //Post
+app.post('/grades', (req, res) => {
+    grades.push(req.body);
+    console.log(req.body);
+    res.end();
+  });
+
+
+  // Delete
+app.delete('/grades/:id',function(req,res){  
+    const filtered= grades.filter(grade=> { return grade.id !== req.params.id });
+    grades=filtered;
+    res.send(grades);
+    res.end();   
+}
+);
+
+
+
+app.listen(3000, function() 
+{console.log('listening on 3000')});
